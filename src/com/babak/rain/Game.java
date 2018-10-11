3c3738034369ec01e29283820fb2b5dd6a7d83d1
buserf <buserf@92.242.132.24>
2018-10-11 03:09:43 +0100

    Episode 4 - added Canvas, Dimension, JFrame, Constructor and main function.  Set up and displayed the JFrame window and running message to the console.

--- src/com/babak/rain/Game.java
+++ src/com/babak/rain/Game.java
@@ -1,19 +1,33 @@
 package com.babak.rain;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
 	public static int width = 300;
 	public static int height = width/16*9;
 	public static int scale = 3;
 	
 	private Thread thread;
+	private JFrame frame;	
 	private boolean running = false;
 	
-	public synchronized void start() {
+	public Game() {
+		Dimension size = new Dimension(width*scale, height*scale);
+		setPreferredSize(size);
+		frame = new JFrame();
+	}
+	
+		public synchronized void start() {
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
-	
+		
 	public synchronized void stop() {
 		running = true;
 		try {
@@ -25,8 +39,21 @@ public class Game implements Runnable {
 
 	public void run() {
 		while (running) {
-			
+			System.out.println("Running!");
 		}
+	}
+	
+	public static void main(String[] args){
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Rain");
+		game.frame.add(game); // game is a subclass of Canvas which is a subclass of Component - we're adding this to our JFrame
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
 	}
 
 }
