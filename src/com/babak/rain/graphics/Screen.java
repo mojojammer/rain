6a62b37ee70ffba721267224b25b6d40d371988d
buserf <buserf@92.242.132.24>
2018-10-12 04:31:50 +0100

    Episode 15 - got rid of xtime/ytime and changed render out of bounds to x & y - also fixed and error with the initial condition on both x&y in out of bounds is now < as opposed to <=
    In the render method - intially we were dividing by 32 (tile size) and
    changed that to /16 - but OPTIMISATION used in final commit to a bitwise
    operation - shifting right by 4 (2^4 = 16) as this is
    faster(?)/optimised(?!) - try changing back and see change in FPS/UPS -
    well not by much actualy!!!  Generally speakinf shiftwise operators are
    faster if working in multiples of 2
    
    Created a tile array and randomly assign a number between 0 (black) and
    )xffffff (white) to each.  Create 32x32 tile size tiles (changed it to
    16 in final code as too clunky with 32 - try it) and render assigning
    color to pixels grouped as tiles

--- src/com/babak/rain/graphics/Screen.java
+++ src/com/babak/rain/graphics/Screen.java
@@ -1,18 +1,24 @@
 package com.babak.rain.graphics;
 
+import java.util.Random;
+
 public class Screen {
 
 	private int width, height;
 	public int[] pixels;
 	
+	public int[] tiles = new int [64 * 64];
 	
-	int counter = 0;
-	private int xtime = 200, ytime = 200;
+	private Random random = new Random();
 	
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
+		
+		for (int i = 0; i < 64 * 64; i++){
+			tiles[i] = random.nextInt(0xffffff);
+		}
 	}
 	
 	public void clear() {
@@ -21,15 +27,12 @@ public class Screen {
 	}
 	
 	public void render() {
-		counter++;
-		if (counter % 10 == 0) xtime--;
-		if (counter % 10 == 0) ytime--;
-		
-		for (int y=0; y < height; y++) {
-			if (ytime <= 0 || ytime >= height) break;
-			for (int x=0; x < width; x++) {
-				if (xtime <= 0 || xtime >= width) break;
-				pixels[xtime + ytime * width] = 0xff00ff;
+		for (int y = 0; y < height; y++) {
+			if (y < 0 || y >= height) break;
+			for (int x = 0; x < width; x++) {
+				if (x < 0 || x >= width) break;
+				int tileIndex = (x >> 4) + (y >> 4) * 64;
+				pixels[x + y * width] = tiles[tileIndex];
 			}
 		}
 	}
