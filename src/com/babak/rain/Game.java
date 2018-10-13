4df63c49bbdd30808bc5e93f3b7127fe5819f4e5
buserf <buserf@92.242.132.24>
2018-10-13 11:50:20 +0100

    Episode 16 - what if we want to move?  Don't need to move rendering area what we should be doing is to move tileIndex!  Added new variables to update tileIndex (I think!?) and constants to easily change size of the map and a mask to reset tabIndex (color) if we go out of bounds on the tile array (our map)!
    Update eclipse project settings to format code style on save

--- src/com/babak/rain/Game.java
+++ src/com/babak/rain/Game.java
@@ -16,32 +16,32 @@ public class Game extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 	public static int width = 300;
-	public static int height = width/16*9;
+	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	public static String title = "Rain";
-		
+
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-	
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
 	private Thread thread;
-	private JFrame frame;	
+	private JFrame frame;
 	private boolean running = false;
-	
+
 	private Screen screen;
-	
+
 	public Game() {
-		Dimension size = new Dimension(width*scale, height*scale);
+		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
 		screen = new Screen(width, height);
 		frame = new JFrame();
 	}
-	
-		public synchronized void start() {
+
+	public synchronized void start() {
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
-		
+
 	public synchronized void stop() {
 		running = true;
 		try {
@@ -58,6 +58,7 @@ public class Game extends Canvas implements Runnable {
 		double delta = 0;
 		int frames = 0;
 		int updates = 0;
+
 		while (running) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
@@ -69,7 +70,7 @@ public class Game extends Canvas implements Runnable {
 			}
 			render();
 			frames++;
-			
+
 			if (System.currentTimeMillis() - timer > 1000) {
 				timer += 1000;
 				System.out.println(updates + " ups, " + frames + " fps");
@@ -78,43 +79,48 @@ public class Game extends Canvas implements Runnable {
 				frames = 0;
 			}
 		}
-		stop();
+		// stop();
 	}
-	
+
+	int x = 0, y = 0;
+
 	public void update() {
-		
+		x++;
+		// y++;
 	}
-	
+
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
 		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
-		
+
 		screen.clear();
-		screen.render();
-		
+		screen.render(x, y);
+
 		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = screen.pixels[i];
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
 		game.frame.setTitle(Game.title);
-		game.frame.add(game); // game is a subclass of Canvas which is a subclass of Component - we're adding this to our JFrame
+		game.frame.add(game); // game is a subclass of Canvas which is a
+								// subclass of Component - we're adding this to
+								// our JFrame
 		game.frame.pack();
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-		
+
 		game.start();
 	}
 
--- src/com/babak/rain/graphics/Screen.java
+++ src/com/babak/rain/graphics/Screen.java
@@ -6,32 +6,39 @@ public class Screen {
 
 	private int width, height;
 	public int[] pixels;
-	
-	public int[] tiles = new int [64 * 64];
-	
+	public final int MAP_SIZE = 3;
+	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
+	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
+
 	private Random random = new Random();
-	
+
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
-		
-		for (int i = 0; i < 64 * 64; i++){
+
+		for (int i = 0; i < MAP_SIZE * MAP_SIZE; i++) {
 			tiles[i] = random.nextInt(0xffffff);
 		}
 	}
-	
+
 	public void clear() {
-		for (int i = 0; i < pixels.length; i++) 
+		for (int i = 0; i < pixels.length; i++)
 			pixels[i] = 0;
 	}
-	
-	public void render() {
+
+	int x = 0, y = 0;
+
+	public void render(int xOffset, int yOffset) {
 		for (int y = 0; y < height; y++) {
-			if (y < 0 || y >= height) break;
+			int yy = y + yOffset;
+			// if (yy < 0 || yy >= height)
+			// break;
 			for (int x = 0; x < width; x++) {
-				if (x < 0 || x >= width) break;
-				int tileIndex = (x >> 4) + (y >> 4) * 64;
+				int xx = x + xOffset;
+				// if (xx < 0 || xx >= width)
+				// break;
+				int tileIndex = ((xx >> 4) & MAP_SIZE_MASK) + ((yy >> 4) & MAP_SIZE_MASK) * MAP_SIZE;
 				pixels[x + y * width] = tiles[tileIndex];
 			}
 		}
