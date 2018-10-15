e133c72794f4263764fb50e59ca864ea540915ff
buserf <buserf@92.242.132.24>
2018-10-15 12:38:11 +0100

    Episode 22 - Moved Res folder to project root (my user error).  Updated the Sprite class and loaded sprite from spritesheet and rendered it to the screen instead of using old tileindex for random colours

--- src/com/babak/rain/graphics/Screen.java
+++ src/com/babak/rain/graphics/Screen.java
@@ -39,7 +39,8 @@ public class Screen {
 				// if (xx < 0 || xx >= width)
 				// break;
 				int tileIndex = ((xx >> 4) & MAP_SIZE_MASK) + ((yy >> 4) & MAP_SIZE_MASK) * MAP_SIZE;
-				pixels[x + y * width] = tiles[tileIndex];
+				// pixels[x + y * width] = tiles[tileIndex];
+				pixels[x + y * width] = Sprite.grass.pixels[(x & 15) + (y & 15) * Sprite.grass.SIZE];
 			}
 		}
 	}
