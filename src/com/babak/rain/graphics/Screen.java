755ff4bb81d89cb707a2517f1c19d3e5b3934674
buserf <buserf@92.242.132.24>
2018-10-16 11:13:47 +0100

    Episode 28 - using offsetting as the tiles are offset depending on where the player moves (WIP) - absolute/relative positioning!  Added renderTile method to the Screen class.

--- src/com/babak/rain/graphics/Screen.java
+++ src/com/babak/rain/graphics/Screen.java
@@ -2,6 +2,8 @@ package com.babak.rain.graphics;
 
 import java.util.Random;
 
+import com.babak.rain.level.tile.Tile;
+
 public class Screen {
 
 	private int width, height;
@@ -40,6 +42,12 @@ public class Screen {
 					continue;
 				pixels[xp + yp * width] = Sprite.grass.pixels[(x & 15) + (y & 15) * Sprite.grass.SIZE];
 			}
+		}
+	}
+
+	public void renderTile(int xp, int yp, Tile tile) {
+		for (int y = 0; y < tile.sprite.SIZE; y++) {
+			int ya = y + yp;
 		}
 	}
 }
