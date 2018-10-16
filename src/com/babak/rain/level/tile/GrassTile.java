27ec4b078d52be150c4fb0d5890019df4224eb79
buserf <buserf@92.242.132.24>
2018-10-16 01:40:46 +0100

    Episode 27 - added a GrassTile class - using Super constructor, obviously not a solid so no need to override solid() Super function but needs a render method.  Similar to the Sprite class we create a static Tile which is GrassTile using the grass Sprite.  By using the GrassTile object for the Tile we are moving a lot of later code out of the Super and into the GrassTile.

---
+++ src/com/babak/rain/level/tile/GrassTile.java
@@ -0,0 +1,16 @@
+package com.babak.rain.level.tile;
+
+import com.babak.rain.graphics.Screen;
+import com.babak.rain.graphics.Sprite;
+
+public class GrassTile extends Tile {
+
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+
+	}
+
+}
