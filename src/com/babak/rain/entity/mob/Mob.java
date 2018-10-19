9e580e3c6a9d2933bfe25dae08ce5854d4beaeb4
buserf <buserf@buserf.com>
2018-10-19 18:24:09 +0100

    Episode 40 - created a new abstract Mob class derived from entity.  This has a sprite and new methods.

---
+++ src/com/babak/rain/entity/mob/Mob.java
@@ -0,0 +1,22 @@
+package com.babak.rain.entity.mob;
+
+import com.babak.rain.entity.Entity;
+import com.babak.rain.graphics.Sprite;
+
+public abstract class Mob extends Entity {
+
+	protected Sprite sprite;
+	protected int dir = 0;
+	protected boolean moving = false;
+
+	public void move() {
+	}
+
+	public void update() {
+	}
+
+	private boolean collision() {
+		return false;
+	}
+
+}
