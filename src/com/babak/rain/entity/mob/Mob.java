d58648912d21e4fa72221bd88e37eb14e4dd6ba5
buserf <buserf@buserf.com>
2018-10-20 15:28:15 +0100

    Episode 42 - need to move player - updated this in the parent Mob class - so all Mobs can move.  Can only move if no collision so we check for that and we also set the direction (dir) to correspond to the direction of movement - will see how dir is used later ... he says :)

--- src/com/babak/rain/entity/mob/Mob.java
+++ src/com/babak/rain/entity/mob/Mob.java
@@ -6,10 +6,23 @@ import com.babak.rain.graphics.Sprite;
 public abstract class Mob extends Entity {
 
 	protected Sprite sprite;
-	protected int dir = 0;
+	protected int dir = 0; // 0=north, 1=east, 2=south 3=east
 	protected boolean moving = false;
 
-	public void move() {
+	public void move(int xa, int ya) {
+		if (xa > 0)
+			dir = 1;
+		if (xa < 0)
+			dir = 3;
+		if (ya > 0)
+			dir = 2;
+		if (ya < 0)
+			dir = 0;
+
+		if (!collision()) {
+			x += xa;
+			y += ya;
+		}
 	}
 
 	public void update() {
