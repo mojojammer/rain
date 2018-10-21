2687ae2f7fb5c8c96a519a200b5cd269f42d250e
buserf <buserf@buserf.com>
2018-10-21 18:41:28 +0100

    Episode 46 - updated Player to draw the other 3 sprites as using a 32x32 sprite for player.  We updated the Player.render() to centre the plaer - took a while to get this but basically think that because we're using a larger sprite for player we need to ship it 16 up and 16 to the left (I think!).  We then get rid of the pink background by updating the Screen.renderplayer method to not show any pink pixels (got colour code from gimp) BUT ... note that our bufferedimage in Game uses RGB which has an alpha channel (not sure taking Cherno's word here - need to research) but our getData on the next line that sets Game.pixels does not so APPARENTY adding an ff at the front of the colour in the Screen.renderplayer() method does in fact do the trick!!

--- src/com/babak/rain/graphics/Screen.java
+++ src/com/babak/rain/graphics/Screen.java
@@ -59,7 +59,9 @@ public class Screen {
 					break;
 				if (xa < 0)
 					xa = 0;
-				pixels[xa + ya * width] = sprite.pixels[x + y * 16];
+				int col = sprite.pixels[x + y * 16];
+				if (col != 0xffff00ff)
+					pixels[xa + ya * width] = col;
 			}
 		}
 	}
