2f95a039a82263bf5e126076af2ac15ba3dbfdf8
buserf <buserf@buserf.com>
2018-10-22 11:40:54 +0100

    Episode 47 - we updated so that we have a single player sprite that is 32x32 and render only a single sprite for the player - i.e. instead of 4 renderplayer calls we now only make one!  This makes very little difference to performance (FPS) as still rendering the same number of pixels but useful later when we come to collisoin - apparently!

--- src/com/babak/rain/graphics/Screen.java
+++ src/com/babak/rain/graphics/Screen.java
@@ -51,15 +51,15 @@ public class Screen {
 	public void renderPlayer(int xp, int yp, Sprite sprite) {
 		xp -= xOffset;
 		yp -= yOffset;
-		for (int y = 0; y < 16; y++) {
+		for (int y = 0; y < 32; y++) {
 			int ya = y + yp; // ya is absolute y position
-			for (int x = 0; x < 16; x++) {
+			for (int x = 0; x < 32; x++) {
 				int xa = x + xp; // absolute x position
-				if (xa < -16 || xa >= width || ya < 0 || ya >= height)
+				if (xa < -32 || xa >= width || ya < 0 || ya >= height)
 					break;
 				if (xa < 0)
 					xa = 0;
-				int col = sprite.pixels[x + y * 16];
+				int col = sprite.pixels[x + y * 32];
 				if (col != 0xffff00ff)
 					pixels[xa + ya * width] = col;
 			}
