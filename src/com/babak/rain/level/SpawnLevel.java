9949cee2dc5c5764ae40fdb3baefdc6269d01847
buserf <buserf@buserf.com>
2018-10-27 13:52:19 +0100

    Episode 53 - updated generateLevel (apparently returning void instead of Tile or Tile[] is quicker(!?) - need to research!).  Not sure how we're going to make three colours dictate how we render tiles and they each currently are made of 2(grass) and 3(flower, rock) colours!  WIP(?)

--- src/com/babak/rain/level/SpawnLevel.java
+++ src/com/babak/rain/level/SpawnLevel.java
@@ -21,6 +21,7 @@ public class SpawnLevel extends Level {
 			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
 			int w = image.getWidth();
 			int h = image.getHeight();
+			tiles = new Tile[w * h];
 			image.getRGB(0, 0, w, h, levelPixels, 0, w);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -29,7 +30,18 @@ public class SpawnLevel extends Level {
 
 	}
 
+	// Grass = 0x04f520
+	// Flower = 0xff0505
+	// Rock = 0xa5a5a5
 	protected void generateLevel() {
+		for (int i = 0; i < levelPixels.length; i++) {
+			if (levelPixels[i] == 0x04f520)
+				tiles[i] = Tile.grass;
+			if (levelPixels[i] == 0xff0505)
+				tiles[i] = Tile.flower;
+			if (levelPixels[i] == 0xa5a5a5)
+				tiles[i] = Tile.rock;
+		}
 
 	}
 }
