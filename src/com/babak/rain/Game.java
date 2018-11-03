dab422fe1e55615e444a16ae651be17f9eda5175
buserf <buserf@buserf.com>
2018-11-03 14:39:11 +0000

    Episode 68 -  cleaned up some of the mess with proper calls and return values from Game.  Also updated Mouse to update angles if we keep the mouse pressed and converted these to degress from radians (when printing - for debug)

--- src/com/babak/rain/Game.java
+++ src/com/babak/rain/Game.java
@@ -24,9 +24,9 @@ public class Game extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 
-	public static int width = 300;
-	public static int height = width / 16 * 9;
-	public static int scale = 3;
+	private static int width = 300;
+	private static int height = width / 16 * 9;
+	private static int scale = 3;
 	public static String title = "Rain";
 
 	private Thread thread;
@@ -64,6 +64,14 @@ public class Game extends Canvas implements Runnable {
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
+	}
+
+	public static int getWindowWidth() {
+		return width * scale;
+	}
+
+	public static int getWindowHeight() {
+		return height * scale;
 	}
 
 	public synchronized void stop() {
--- src/com/babak/rain/entity/mob/Player.java
+++ src/com/babak/rain/entity/mob/Player.java
@@ -1,5 +1,6 @@
 package com.babak.rain.entity.mob;
 
+import com.babak.rain.Game;
 import com.babak.rain.graphics.Screen;
 import com.babak.rain.graphics.Sprite;
 import com.babak.rain.input.Keyboard;
@@ -48,8 +49,8 @@ public class Player extends Mob {
 
 	private void updateShooting() {
 		if (Mouse.getButton() == 1) {
-			double dx = Mouse.getX() - 300 / 2;
-			double dy = Mouse.getY() - 168 / 2;
+			double dx = Mouse.getX() - Game.getWindowWidth() / 2;
+			double dy = Mouse.getY() - Game.getWindowHeight() / 2;
 			double dir = Math.atan2(dy, dx);
 			shoot(x, y, dir);
 		}
